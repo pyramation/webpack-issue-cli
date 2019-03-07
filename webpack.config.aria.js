0f92959d3839c1b93b73b5ac46536a57f7d7c7ef
Dan Lynch <pyramation@gmail.com>
2019-03-06 22:07:11 -0800

    first commit

---
+++ webpack.config.aria.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: [
+    "aria",
+    "autoprefixer",
+    "react",
+    "react-dom",
+    "redux",
+    "aria/aria.js",
+    "react/index.js",
+    "react-dom/index.js",
+    "react-dom/profiling.js",
+    "react-dom/server.browser.js",
+    "react-dom/server.js",
+    "react-dom/server.node.js",
+    "react-dom/test-utils.js",
+    "react-dom/unstable-native-dependencies.js"
+  ],
+  output: {
+    path: __dirname,
+    filename: "w-aria.js"
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: [
+    "autoprefixer",
+    "react",
+    "react-dom",
+    "redux",
+    "react/index.js",
+    "react-dom/index.js",
+    "react-dom/profiling.js",
+    "react-dom/server.browser.js",
+    "react-dom/server.js",
+    "react-dom/server.node.js",
+    "react-dom/test-utils.js",
+    "react-dom/unstable-native-dependencies.js"
+  ],
+  output: {
+    path: __dirname,
+    filename: "no-aria.js"
+  }
+};
